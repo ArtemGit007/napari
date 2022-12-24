7ce77d4a4a3053b1fdda447835df511e1b064dda
ArtemGit007 <119147958+ArtemGit007@users.noreply.github.com>
2022-12-24 23:12:32 +0300

    форматирование текста

--- examples/3Dimage_plane_rendering.py
+++ examples/3Dimage_plane_rendering.py
@@ -15,22 +15,26 @@ from skimage import data
 viewer = napari.Viewer(ndisplay=3)
 
 # add a 3D image
-blobs = data.binary_blobs(
+blobs = data.binary_blobs
+(
     length=64, volume_fraction=0.1, n_dim=3
 ).astype(np.float32)
-image_layer = viewer.add_image(
+image_layer = viewer.add_image
+(
     blobs, rendering='mip', name='volume', blending='additive', opacity=0.25
 )
 
 # add the same 3D image and render as plane
 # plane should be in 'additive' blending mode or depth looks all wrong
-plane_parameters = {
+plane_parameters = 
+{
     'position': (32, 32, 32),
     'normal': (0, 1, 0),
     'thickness': 10,
 }
 
-plane_layer = viewer.add_image(
+plane_layer = viewer.add_image
+(
     blobs,
     rendering='average',
     name='plane',
